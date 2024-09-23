5e5c0931e3f52a56cf71d68619c61b12276a8b52
TwinMine <michaelrolo11@gmail.com>
2024-09-23 17:18:50 +0200

    add music-player

--- src/components/musicPlayer/MusicPlayer.jsx
+++ src/components/musicPlayer/MusicPlayer.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import ReactPlayer from "react-player";
+import "./musicPlayer.css"
+
 const MusicPlayer = () => {
-    return(
-        <>
-        Music Player
-        </>
-    )
-}
+  const [url, setUrl] = useState("");
+
+  const musicContainer = [
+    { english: "https://www.youtube.com/watch?v=dCVTpeceIew" },
+    { german: "https://www.youtube.com/watch?v=LUlZ5n0cyak" }, 
+    { japanese: "https://www.youtube.com/watch?v=jbHG7fsZVkM" },
+  ];
+
+  return (
+    <div>
+        <div className="player">
+         <ReactPlayer url={url} playing controls loop />   
+        </div>
+      
+
+      <button onClick={() => setUrl(musicContainer[0].english)}>English</button>
+      <button onClick={() => setUrl(musicContainer[1].german)}>German</button>
+      <button onClick={() => setUrl(musicContainer[2].japanese)}>Japanese</button>
+    </div>
+  );
+};
 
 export default MusicPlayer;
